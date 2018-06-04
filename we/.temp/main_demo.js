2fa149e8ac3c917e83a0aad9809fa8282ebe0639
goldze <goldze@126.com>
2018-06-04 13:02:09 +0800

    Initial commit
    Initial commit

---
+++ we/.temp/main_demo.js
@@ -0,0 +1,3 @@
+import App from '..\\src\\main_demo.vue'
+App.el = '#root'
+new Vue(App)
---
+++ we/.temp/main_demo.web.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import weex from 'weex-vue-render';
+
+weex.init(Vue);
+
+
+
+const App = require('..\\src\\main_demo.vue');
+new Vue(Vue.util.extend({el: '#root'}, App));
