b666f41fcac08513d061d30ccfa2d995c3c2fd39
goldze <goldze@126.com>
2018-09-16 19:57:35 +0800

    supplement
    supplement

--- android/app/src/main/java/com/goldze/weex/MainActivity.java
+++ android/app/src/main/java/com/goldze/weex/MainActivity.java
@@ -3,7 +3,6 @@ package com.goldze.weex;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import java.util.HashMap;
 
 import me.goldze.weex.ui.WeexPagerActivity;
